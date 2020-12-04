6fb9287b0c5140b962f5931a11cc743b55d8e7b2
Leandro Costa <lscosta@gmail.com>
2020-12-04 20:26:54 -0300

    .

--- modem.py
+++ modem.py
@@ -1,5 +1,12 @@
 from config import settings
 import modem_uteis as mu
+import json
 
-mu.modem_tim(settings.link_webdriver, settings.link, settings.password)
+mu.modem_tim(
+            link_webdriver=settings.link_webdriver, 
+            link=settings.link_modem_timlive, 
+            password=settings.password_modem_timlive,
+            operadora ="timlive",
+            tempo_debug = 2
+)
 
--- modem_uteis.py
+++ modem_uteis.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
 import time
 
-def modem_tim(link_webdriver, link, password):
+def tempo_conexao_dict():
+    tempo_modem_ligado = "2 dias 6 horas 15 min 47 seg"
+    tempo_conectado_dsl = tempo_modem_ligado.split(" ")
+    tempo_conectado_dsl = (f''' {{"dias":{tempo_conectado_dsl[0]},"horas":{tempo_conectado_dsl[2]},"min":{tempo_conectado_dsl[4]},"seg":{tempo_conectado_dsl[6]}}}''')
+    tempo_conectado_dsl_dict = eval(tempo_conectado_dsl)
+
+
+def modem_tim(link_webdriver, link, password, operadora, tempo_debug):
 
     capabilities = {
         "browserName": "chrome",
@@ -18,40 +25,42 @@ def modem_tim(link_webdriver, link, password):
 
     browser.get(link)
 
-    #digita a senha    
-    passwordbox = browser.find_element_by_xpath('//*[@id="srp_password"]')
-    passwordbox.send_keys(password)
-    time.sleep(1)
-    #clica no botão entra
-    entrarButton = browser.find_element_by_xpath('//*[@id="sign-me-in"]')
-    entrarButton.click()
-    time.sleep(1)
-    # click na engranem para pegar options
-    bandalargacfg = browser.find_element_by_xpath('//*[@id="Banda Larga"]/i')
-    # bandalargacfg = browser.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/div/div[1]/div[3]/i')
-    bandalargacfg.click()
-    time.sleep(1)
+    if operadora == "timlive":
+        #digita a senha    
+        passwordbox = browser.find_element_by_xpath('//*[@id="srp_password"]')
+        passwordbox.send_keys(password)
+        time.sleep(1)
+        #clica no botão entra
+        entrarButton = browser.find_element_by_xpath('//*[@id="sign-me-in"]')
+        entrarButton.click()
+        time.sleep(1)
+        # click na engranem para pegar options
+        bandalargacfg = browser.find_element_by_xpath('//*[@id="Banda Larga"]/i')
+        # bandalargacfg = browser.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/div/div[1]/div[3]/i')
+        bandalargacfg.click()
+        time.sleep(1)
 
-    status_id = browser.find_element_by_id('Status DSL')
+        status_id = browser.find_element_by_id('Status DSL')
 
-    status_sinal = status_id.text
+        status_sinal = status_id.text
 
-    print("")
-    print(f"Link da TimLive:")
-    # print("")
+        print("")
+        print(f"Link da TimLive:")
+        # print("")
 
-    # print(type(status_sinal))
-    if status_sinal == "NoSignal":
-        print(f"O link está:.............. {status_id.text}")
-    else:
-        tempo_conectado_dsl = browser.find_element_by_id('Tempo conectado DSL')
-        line_rate = browser.find_element_by_id('Line Rate')
-        print(f"O link está:.............. {status_id.text}")
-        print(f"O tempo é:................ {tempo_conectado_dsl.text}")
-        print(f"Conexão DSL velocidade:... {line_rate.text}")
+        # print(type(status_sinal))
+        if status_sinal == "NoSignal":
+            print(f"O link está:.............. {status_id.text}")
+        else:
+            tempo_conectado_dsl = browser.find_element_by_id('Tempo conectado DSL')
+            line_rate = browser.find_element_by_id('Line Rate')
+            print(f"O link está:.............. {status_id.text}")
+            print(f"O tempo é:................ {tempo_conectado_dsl.text}")
+            print(f"Conexão DSL velocidade:... {line_rate.text}")
+            # print(f"tempo tipo: {type(tempo_conectado_dsl.text)}")
 
-    time.sleep(1)
+        time.sleep(tempo_debug)
 
-    browser.quit()
+        browser.quit()
 
 
